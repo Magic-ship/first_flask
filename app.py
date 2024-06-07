e335303decb2edeecc6c20736f833d4b0626659c
feeling-free <tadashimamoto@gmail.com>
2024-06-08 06:08:52 +0900

    Set dynamic routing

--- app.py
+++ app.py
@@ -5,10 +5,14 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Welcome to my first flask app!"
-    
+
 @app.route("/educative")
 def learn():
-    return "I am happy to learning at educative!"
+    return "I am learning at Educative"
+
+@app.route("/square/<int:number>")
+def show_square(number):
+    return "Square of " + str(number) + " is: " + str(number * number)
 
 if __name__ == "__main__":
     app.run(debug = True, host = '0.0.0.0', port = 3000)
