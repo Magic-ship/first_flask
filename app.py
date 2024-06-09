94666fc50cc13c6a93d7aa711b2d0b3664a62fbf
feeling-free <tadashimamoto@gmail.com>
2024-06-09 18:47:14 +0900

    Load page using template file

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Welcome to my first flask app!"
-
-@app.route("/educative")
-def learn():
-    return "I am learning at Educative"
-
-@app.route("/square/<int:number>")
-def show_square(number):
-    return "Square of " + str(number) + " is: " + str(number * number)
+def home():
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug = True, host = '0.0.0.0', port = 3000)
